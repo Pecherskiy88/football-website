6af6e177c80d0268beca4ef5299e3c67b26a1092
Pecherskiy88 <pecherskiy88@gmail.com>
2019-05-28 12:13:54 +0300

    start do header

---
+++ src/Components/Header_footer/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  render() {
+    return <div>Header</div>;
+  }
+}
+
+export default Header;
+// 22 lesson 9:20
